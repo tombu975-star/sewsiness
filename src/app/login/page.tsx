318fc6365b86aa86e62c6a24467e880268edf115
tombu975-star <tombu975@gmail.com>
2026-08-31 12:34:38 +0000

    Add files via upload

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,16 @@
 import { Suspense } from "react";
 import { LoginForm } from "./LoginForm";
+import { LoginSplash } from "./LoginSplash";
 import { getPlatformSettings } from "@/lib/platform-settings";
 
 export default async function LoginPage() {
   const platform = await getPlatformSettings();
+
   return (
-    <Suspense fallback={null}>
-      <LoginForm platform={platform} />
-    </Suspense>
+    <LoginSplash platform={platform}>
+      <Suspense fallback={null}>
+        <LoginForm platform={platform} />
+      </Suspense>
+    </LoginSplash>
   );
 }
